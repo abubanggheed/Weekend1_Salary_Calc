47bd6dcfa03a594f1dc783f5e48376ed50f5c91d
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-08-31 15:23:21 -0500

    setup html and basics of submit button

---
+++ client.js
@@ -0,0 +1,12 @@
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('ready');
+    $('#addEmployeeButton').on('click', handleAddEmployee);
+}//end onReady
+
+function handleAddEmployee(){
+    console.log('adding employee');
+    
+}
