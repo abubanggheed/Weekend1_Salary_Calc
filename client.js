98e9cfe2db964a34d98ed5cb51d1fcbf05c622c5
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-08-31 16:48:47 -0500

    employee array working but badly

--- client.js
+++ client.js
@@ -1,33 +1,41 @@
 
 $(document).ready(onReady);
 
+//global variables
 let monthlyPayout;
+let employees = [];
 
 function onReady() {
-    console.log('ready');
+    //set up events
     $('#addEmployeeButton').on('click', handleAddEmployee);
     $('#employeeList').on('click', '.removeEmployeeButton', handleRemoveEmployee);
+    //finish dom
     updateMonthlyPayout();
 }//end onReady
 
-function handleAddEmployee() {
+function handleAddEmployee() {//- Submit Button
+    //get values from inputs
     let fName = $('#firstNameIn').val();
     let lName = $('#lastNameIn').val();
     let ID = $('#employeeIDIn').val();
     let title = $('#titleIn').val();
     let salary = $('#salaryIn').val();
     if(fName && lName && ID && title && salary){
+        //build a new table entry string
         $('#employeeList').append(`
             <tr>
                 <td>${fName}</td>
                 <td>${lName}</td>
-                <td>${ID}</td>
+                <td class="findID">${ID}</td>
                 <td>${title}</td>
                 <td class="findSalary">${salary}</td>
                 <td><button class="removeEmployeeButton">Remove</button></td>
             </tr>
         `);
+        //clear inputs
         $('#inputDiv').children('input').val('');
+        //add new employee to array
+        employees.push( new Employee(fName, lName, ID, title, salary));
         updateMonthlyPayout();
     }
 
@@ -35,22 +43,52 @@ function handleAddEmployee() {
 
 function updateMonthlyPayout(){
     monthlyPayout = 0;
+    //obtain array of salaries as strings
     let salaries = $('.findSalary');
+    //sum salaries
     for(salary of salaries){
         monthlyPayout += parseInt(salary.textContent);
     }
+    //do some uninspiring work
     monthlyPayout = (monthlyPayout / 12);
     if(monthlyPayout > 20000){
         $('#totalMonthlyLine').css('color', 'red');
     }
     monthlyPayout = monthlyPayout.toFixed(2);
+    //put total on dom
     $('#totalMonthly').text(monthlyPayout);
 }//end updateMonthlyPayout
 
-function handleRemoveEmployee(){
-    $(this).closest('tr').remove();
+function handleRemoveEmployee(){//-Remove Button
+    //get row
+    let row = $(this).closest('tr');
+    //find ID number
+    let ID = row.children()[2].innerHTML;
+    //remove employee from array
+    removeFromArray(ID);
+    //remove row that contains button
+    row.remove();
     updateMonthlyPayout();
+    //let monthly salary return to being black
     if(monthlyPayout <= 20000){
         $('#totalMonthlyLine').css('color', 'black');
+    }
+}
+
+function removeFromArray(ID){
+    for(let i in employees){
+        if (employees[i].ID === ID){
+            employees.splice(i, 1)
+        }
+    }
+}//end removeFromArray
+
+class Employee{
+    constructor(fName, lName, ID, Title, Salary){
+        this.firstName = fName;
+        this.lastName = lName;
+        this.ID = ID;
+        this.title = Title;
+        this.salary = Salary;
     }
 }
