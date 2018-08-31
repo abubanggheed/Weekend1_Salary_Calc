c292a3373f928663c293601cb38f2ed66503672e
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-08-31 15:50:07 -0500

    employees are addeable and the monthly wages are automatically calculated

--- client.js
+++ client.js
@@ -1,12 +1,43 @@
 
 $(document).ready(onReady);
 
-function onReady(){
+let monthlyPayout;
+
+function onReady() {
     console.log('ready');
     $('#addEmployeeButton').on('click', handleAddEmployee);
+    updateMonthlyPayout();
 }//end onReady
 
-function handleAddEmployee(){
-    console.log('adding employee');
-    
-}
+function handleAddEmployee() {
+    let fName = $('#firstNameIn').val();
+    let lName = $('#lastNameIn').val();
+    let ID = $('#employeeIDIn').val();
+    let title = $('#titleIn').val();
+    let salary = $('#salaryIn').val();
+    if(fName && lName && ID && title && salary){
+        $('#employeeList').append(`
+            <tr>
+                <td>${fName}</td>
+                <td>${lName}</td>
+                <td>${ID}</td>
+                <td>${title}</td>
+                <td class="findSalary">${salary}</td>
+                <td><button class="removeEmployeeButton">Remove</button></td>
+            </tr>
+        `);
+        $('#inputDiv').children('input').val('');
+        updateMonthlyPayout();
+    }
+
+}//end handleAddEmployee
+
+function updateMonthlyPayout(){
+    monthlyPayout = 0;
+    let salaries = $('.findSalary');
+    for(salary of salaries){
+        monthlyPayout += parseInt(salary.textContent);
+    }
+    monthlyPayout = (monthlyPayout / 12).toFixed(2);
+    $('#totalMonthly').text(monthlyPayout)
+}//end updateMonthlyPayout
